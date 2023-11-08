6ebef6a38d3448bd3d0d15433dbbe538c572d624
Gabriel Galdino <gabrielgaldinodev@gmail.com>
2023-11-08 15:23:40 -0300

    criado atributo de classe olho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,5 @@
 class Pessoa:
+    olhos = 2
     def __init__(self, *filhos, nome=None, idade=26):
         self.idade = idade
         self.nome = nome
@@ -18,5 +19,11 @@ if __name__ == '__main__':
         print(filho.nome)
     Gabriel.sobrenome = 'Galdino'
     del Gabriel.filhos
+    valentina.olhos=1
+    del valentina.olhos
     print(Gabriel.__dict__)
     print(valentina.__dict__)
+    Pessoa.olhos=3
+    print(Pessoa.olhos)
+    print(Gabriel.olhos)
+    print(valentina.olhos)
