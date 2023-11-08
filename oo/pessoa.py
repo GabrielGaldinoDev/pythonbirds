e9f0b10fbb2cff9d9f6527670d259acd2bbd6425
Gabriel Galdino <gabrielgaldinodev@gmail.com>
2023-11-08 14:47:57 -0300

    criado atributo complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,18 @@
 class Pessoa:
+    def __init__(self, *filhos, nome=None, idade=26):
+        self.idade = idade
+        self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    valentina = Pessoa(nome='valentina')
+    Gabriel = Pessoa(valentina, nome='gabriel')
+    print(Pessoa.cumprimentar(Gabriel))
+    print(id(Gabriel))
+    print(Gabriel.cumprimentar())
+    print(Gabriel.nome)
+    print(Gabriel.idade)
+    for filho in Gabriel.filhos:
+        print(filho.nome)
