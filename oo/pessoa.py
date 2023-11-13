f276ecb4f87906bedb0ad41704c639c85bb2981a
Gabriel Galdino <gabrielgaldinodev@gmail.com>
2023-11-13 14:21:23 -0300

    IMPLEMENTAÇÃO DA DIREÇÃO

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -6,6 +6,13 @@ class Pessoa:
         self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Olá {id(self)}'
+    @staticmethod
+    def metodo_estatico():
+        return 42
+
+    @classmethod
+    def nome_e_atributo_de_classe(cls):
+        return f'{cls} - olhos {cls.olhos}'
 
 if __name__ == '__main__':
     valentina = Pessoa(nome='valentina')
@@ -27,3 +34,5 @@ if __name__ == '__main__':
     print(Pessoa.olhos)
     print(Gabriel.olhos)
     print(valentina.olhos)
+    print((Pessoa.metodo_estatico(),Gabriel.metodo_estatico()))
+    print((Pessoa.nome_e_atributo_de_classe(),Gabriel.nome_e_atributo_de_classe()))
