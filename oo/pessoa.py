3546dd423a2980ee99dc830ee5538523ecc372c5
Gabriel Galdino <gabrielgaldinodev@gmail.com>
2023-11-22 15:41:09 -0300

    PROJETO FINAL

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -5,7 +5,7 @@ class Pessoa:
         self.nome = nome
         self.filhos = list(filhos)
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá, meu nome é {self.nome}'
     @staticmethod
     def metodo_estatico():
         return 42
@@ -14,9 +14,16 @@ class Pessoa:
     def nome_e_atributo_de_classe(cls):
         return f'{cls} - olhos {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_classe=super().cumprimentar()
+        return f'{cumprimentar_da_classe}. Aperto de mão'
+class Mutante(Pessoa):
+    olhos = 3
+
 if __name__ == '__main__':
-    valentina = Pessoa(nome='valentina')
-    Gabriel = Pessoa(valentina, nome='gabriel')
+    valentina = Mutante(nome='valentina')
+    Gabriel = Homem(valentina, nome='gabriel')
     print(Pessoa.cumprimentar(Gabriel))
     print(id(Gabriel))
     print(Gabriel.cumprimentar())
@@ -30,9 +37,11 @@ if __name__ == '__main__':
     del valentina.olhos
     print(Gabriel.__dict__)
     print(valentina.__dict__)
-    Pessoa.olhos=3
     print(Pessoa.olhos)
     print(Gabriel.olhos)
     print(valentina.olhos)
     print((Pessoa.metodo_estatico(),Gabriel.metodo_estatico()))
     print((Pessoa.nome_e_atributo_de_classe(),Gabriel.nome_e_atributo_de_classe()))
+    print(valentina.olhos)
+    print(Gabriel.cumprimentar())
+    print(valentina.cumprimentar())
